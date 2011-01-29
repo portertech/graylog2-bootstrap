f0a92750ff32279b4f6b9cc51be687c80b66ccdb
Sean Porter <portertech@gmail.com>
2011-01-29 12:50:39 -0800

    get graylog2-server running

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ release_src=https://github.com/downloads/Graylog2
 graylog2_server=graylog2-server-0.9.4p1.tar.gz
 graylog2_web_interface=graylog2-web-interface-0.9.4p2.tar.gz
 graylog2_base=/var/graylog2
+graylog2_collection_size=650000000
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -33,25 +34,30 @@ sudo ln -sf $graylog2_base/src/$folder $graylog2_base/web
 
 sudo gem install rubygems-update
 sudo /var/lib/gems/1.8/bin/update_rubygems
+sudo gem install bundler
 
-for gem in bundler bluepill
-do
-  sudo gem install $gem
-done
+cd $graylog2_base/server
 
-sudo mv -f $graylog2_base/server/graylog2.conf.example $graylog2_base/server/graylog2.conf
+sudo mv -f graylog2.conf.example graylog2.conf
+sudo sed -e "s/true/false/" -i graylog2.yml
+sudo sed -e "s/50000000/$graylog2_collection_size/" -i graylog2.yml
 sudo ln -s $graylog2_base/server/graylog2.conf /etc/graylog2.conf
+
+cd bin/ && sudo ./graylog2ctl start
 
 cd $graylog2_base/web
 
 sudo bundle install
 
+sudo sed -e "s/yourpass//" -i config/database.yml
 fqdn=`hostname --fqdn`
-
-sudo sed -e "s/root/graylog2/g" -i config/database.yml
-sudo sed -e "s/yourpass/Gr4yl0g2p455wD/g" -i config/database.yml
 sudo sed -e "s/your-graylog2.example.org/$fqdn/g" -i config/general.yml
 
+export RAILS_ENV=production
+
 sudo chown -R nobody:nogroup $graylog2_base
+
+rake db:create
+rake db:migrate
 
 exit 0
